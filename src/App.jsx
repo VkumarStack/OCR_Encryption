c906e7c8b9730ec0f2244100a701306b43852bb8
VkumarStack <vksogi@gmail.com>
2023-04-01 15:51:52 -0700

    Fix PDF line spacing

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,24 @@ import { isMobile } from 'react-device-detect';
 import jsPDF from 'jspdf';
 import "./OldNewspaperTypes-normal"
 
+// Assumes your document using is `pt` units
+// If you're using any other unit (mm, px, etc.) use this gist to translate: https://gist.github.com/AnalyzePlatypus/55d806caa739ba6c2b27ede752fa3c9c
+function addWrappedText({text, textWidth, doc, fontSize = 10, fontType = 'normal', lineSpacing = 7, xPosition = 10, initialYPosition = 10, pageWrapInitialYPosition = 10}) {
+  doc.setFontSize(fontSize);
+  var textLines = doc.splitTextToSize(text, textWidth); // Split the text into lines
+  var pageHeight = doc.internal.pageSize.height;        // Get page height, we'll use this for auto-paging. TRANSLATE this line if using units other than `pt`
+  var cursorY = initialYPosition;
+
+  textLines.forEach(lineText => {
+    if (cursorY > pageHeight - 10) { // Auto-paging
+      doc.addPage();
+      cursorY = pageWrapInitialYPosition;
+    }
+    doc.text(xPosition, cursorY, lineText);
+    cursorY += lineSpacing;
+  })
+}
+
 function App() {
   const [image, setImage] = useState(null);
   const workerRef = useRef(null);
@@ -86,9 +104,14 @@ function App() {
         if (outputRef.current.defaultValue.length > 0) {
           const doc = new jsPDF();
           doc.setFont('OldNewspaperTypes', 'normal');
-          doc.text(outputRef.current.defaultValue, 10, 10);
+          addWrappedText({
+            text: outputRef.current.defaultValue, 
+            textWidth: 180,
+            doc,
+            fontSize: '12',
+          })
+          //doc.text(outputRef.current.defaultValue, 10, 10);
           doc.save("output.pdf");
-          console.log(doc);
         }
       }}> Save as PDF </button>
     </div>
